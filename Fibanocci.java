189a764b71296d5544add16d652219efaa5ae2f6
Askin raj <112162087+Askinraj@users.noreply.github.com>
2022-10-15 12:49:28 +0530

    Add files via upload

---
+++ Fibanocci.java
@@ -0,0 +1,20 @@
+package apti;
+
+public class Fibanocci {
+
+	public static void main(String[] args) {
+		int num1 = 0;
+		int num2 =1;
+		int fiba;
+		for(int i=0;i<10;i++)
+		{  System.out.println(num1);
+			 fiba=num1+num2;
+			num1=num2;
+			num2=fiba;
+	
+			
+		}
+ 
+	}
+
+}
